d935f864416e89dcedd4c9cd2dee32e0e32cc6f2
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-12-21 15:32:12 +0700

    fix version

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import storageRoute from './storage-route';
 
+import googleDriveRoute from './google-drive-route';
+
 const basePath = '/api/v1';
 
 
 export const routes = [
     { path: `${basePath}/s3`, handler: storageRoute },
+    { path: `${basePath}/bucket`, handler: googleDriveRoute },
 ];
